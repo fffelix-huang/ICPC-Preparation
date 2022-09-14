f7caf13d80e8d7e2c856d6cf981cdc093bc5468c
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-09-15 00:39:27 +0800

    Update Treap.cpp

--- Codebook/Data-Structure/Treap.cpp
+++ Codebook/Data-Structure/Treap.cpp
@@ -8,28 +8,23 @@ int node(int x){
 }
 void up(int x){
     sz[x]=1+sz[ch[x][0]]+sz[ch[x][1]];
-    
 }
 int merge(int a,int b){
     if(!a||!b) return a|b;
     if(pri[a]<pri[b]){
         ch[a][1]=merge(ch[a][1],b);
-        up(a);
-        return a;
+        up(a);  return a;
     }else{
         ch[b][0]=merge(a,ch[b][0]);
-        up(b);
-        return b;
+        up(b);  return b;
     }
 }
 void split(int p,int &a,int &b,int k){
     if(!p) return a=b=0,void();
     if(val[p]<k){
-        a=p;
-        split(ch[p][1],ch[a][1],b,k);
+        a=p;  split(ch[p][1],ch[a][1],b,k);
     }else{
-        b=p;
-        split(ch[p][0],a,ch[b][0],k);
+        b=p;  split(ch[p][0],a,ch[b][0],k);
     }
     up(p);
 }
